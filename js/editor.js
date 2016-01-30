8145dbd03ed7f34fd11faf6af3c8654a053b09c1
Eddy Gao <ewgao@uwaterloo.ca>
2016-01-30 01:20:37 -0500

    deleting almost works

--- js/editor.js
+++ js/editor.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
     $('#ask-btn').click(function() {
-        activeNode = "<div class='ask block'><input class='cond' type='text' value='1==0'>?<div class='yes'>"+addNode+"</div><div class='no'>"+addNode+"</div></div>";
+        activeNode = "<div class='ask block'><span class='delete' onclick='delClick(this)'>X</span><input class='cond' type='text' value='1==0'>?<div class='yes'>"+addNode+"</div><div class='no'>"+addNode+"</div></div>";
         $('.add').toggleClass('active');
     });
     $('#for-btn').click(function() {
-        activeNode = "<div class='for block'>Repeat <input class='val for-inp' type='text' value='2'> times<div class='loop'>"+addNode+"</div></div>";
+        activeNode = "<div class='for block'><span class='delete' onclick='delClick(this)'>X</span>Repeat <input class='val for-inp' type='text' value='2'> times<div class='loop'>"+addNode+"</div></div>";
         $('.add').toggleClass('active');
     });
     $('#repeat-btn').click(function() {
-        activeNode = "<div class='repeat block'><input class='cond' type='text' value='1==0'>:<div class='loop'>"+addNode+"</div></div>";
+        activeNode = "<div class='repeat block'><span class='delete' onclick='delClick(this)'>X</span><input class='cond' type='text' value='1==0'>:<div class='loop'>"+addNode+"</div></div>";
         $('.add').toggleClass('active');
     });
     $('#print-btn').click(function() {
-        activeNode = "<div class='print block'></div>";
+        activeNode = "<div class='print block'><span class='delete' onclick='delClick(this)'>X</span></div>";
         $('.add').toggleClass('active');
     });
     $('#assign-btn').click(function() {
-        activeNode = "<div class='assign block'><input class='val' type='text' placeholder='post-it note = value' val=''></div>";
+        activeNode = "<div class='assign block'><span class='delete' onclick='delClick(this)'>X</span><input class='val' type='text' placeholder='post-it note = value' val=''></div>";
         $('.add').toggleClass('active');
     })
     $('#editor').html(addNode);
@@ -35,4 +35,7 @@ function addClick(t) {
         $('.add').toggleClass('active');
         $(t).replaceWith(withAdd(activeNode));
     }
+}
+function delClick(t) {
+    $(t).parent().remove();
 }
